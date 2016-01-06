2806e8e1a4512d99545c9bd8005c1821e16fb6db
Joe Adams <josephadams9@gmail.com>
2016-01-05 21:29:14 -0500

    Implemented Debug renderer

--- src/box2d_debug_drawer.py
+++ src/box2d_debug_drawer.py
@@ -8,11 +8,11 @@ class Box2dDebugDrawer(b2DrawExtended):
     https://github.com/pybox2d/pybox2d/blob/master/examples/backends/pygame_framework.py
     """
 
-    def __init__(self, surface, **kwargs):
+    def __init__(self, renderer, **kwargs):
         b2DrawExtended.__init__(self, **kwargs)
 
-        # The pygame surface to draw to
-        self.surface = surface
+        # The renderer to use
+        self.renderer = renderer
         
         self.flipX = False
         self.flipY = True
@@ -28,26 +28,53 @@ class Box2dDebugDrawer(b2DrawExtended):
 
 
     def DrawPoint(self, p, size, color):
-        print("Draw point")
+        self.DrawCircle(p, size, color)
     
     def DrawAABB(self, aabb, color):
         print("Draw aabb")
+        points = [(aabb.lowerBound.x, aabb.lowerBound.y),
+                  (aabb.upperBound.x, aabb.lowerBound.y),
+                  (aabb.upperBound.x, aabb.upperBound.y),
+                  (aabb.lowerBound.x, aabb.upperBound.y)]
 
+        self.renderer.drawLines(points, color)
     def DrawSegment(self, p1, p2, color):
-        print("Draw segment")
+        self.renderer.drawLine(p1, p2, color.bytes)
 
     def DrawTransform(self, xf):
-        print("Draw Transform")
+        p1 = xf.position
+        p2 = self.to_screen(p1 + 1 * xf.R.x_axis)
+        p3 = self.to_screen(p1 + 1 * xf.R.y_axis)
+        p1 = self.to_screen(p1)
+
+        self.renderer.drawLine(p1, p2, (255, 0, 0))
+        self.renderer.drawLine(p1, p3, (0, 255, 0))
     
     def DrawCircle(self, center, radius, color, drawwidth=1):
-        print("Draw Circle")
+        radius = int(radius)
+        self.renderer.drawCircle(center, radius, color.bytes)
 
     def DrawSolidCircle(self, center, radius, axis, color):
-        print("Draw Solid Circle")
+        radius = int(radius)
+        self.renderer.drawFilledCircle(center, radius, color)
+        self.renderer.drawLine(center,
+                               (center[0] - radius * axis[0],
+                                center[1] + radius * axis[1]),
+                                (255, 0, 0))
 
-    def DrawPolygon(self, verticies, color):
-        print("Draw Polygon")
+    def DrawPolygon(self, vertices, color):
+        if not vertices:
+            return
+        if len(vertices) == 2:
+            self.DrawSegment(vertices[0], vertices[1], color)
+        else:
+            self.renderer.drawPolygon(vertices, color.bytes)
 
-    def DrawSolidPolygon(self, verticies, color):
-        print("Draw Solid Polygon")
+    def DrawSolidPolygon(self, vertices, color):
+        if not vertices:
+            return
+        if len(vertices) == 2:
+            self.DrawSegment(vertices[0], vertices[1], color)
+        else:
+            self.renderer.drawFilledPolygon(vertices, color.bytes)
 
--- src/renderer.py
+++ src/renderer.py
@@ -6,15 +6,33 @@ from box2d_debug_drawer import Box2dDebugDrawer
 class Renderer:
     
     def __init__(self):
-        self.windowSurface = pygame.display.set_mode((640, 480))
+        self.surface = pygame.display.set_mode((640, 480))
         
     def gameWillRender(self):
-        self.windowSurface.fill((255, 255, 255))
+        self.surface.fill((255, 255, 255))
 
     def gameDidRender(self):
         pygame.display.flip()
 
     def setupDebugDraw(self, world):
-        self.debugDrawer = Box2dDebugDrawer(self.windowSurface)
+        self.debugDrawer = Box2dDebugDrawer(self)
         world.renderer = self.debugDrawer
-        
+
+    def drawLine(self, p1, p2, color):
+        pygame.draw.aaline(self.surface, color, p1, p2)
+
+    def drawLines(self, points, color):
+        pygame.draw.lines(self.surface, color, True, points)
+
+    def drawPolygon(self, vertices, color):
+        pygame.draw.polygon(self.surface, color, vertices, 1)
+
+    def drawFilledPolygon(self, vertices, color):
+        pygame.draw.polygon(self.surface, color, vertices)
+
+    def drawCircle(self, center, radius, color):
+        pygame.draw.circle(self.surface, color,
+                           center, radius, 1)
+    def drawFilledCircle(self, center, radius, color):
+        pygame.draw.circle(self.surface, color,
+                           center, radius, 0)
